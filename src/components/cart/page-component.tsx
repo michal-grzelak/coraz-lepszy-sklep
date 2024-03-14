7e1a7853fe43815fd8a236113071de6e49975fd2
Michał Grzelak <grzelakm@outlook.com>
2024-03-14 11:13:45 +0100

    Add product removing

--- src/components/cart/page-component.tsx
+++ src/components/cart/page-component.tsx
@@ -1,17 +1,20 @@
 "use client"
 
 import { AddProductForm } from "@components/product/add-product-form"
-import { CartItem } from "@components/product/cart-item"
+import { CartItemInteractive } from "@components/product/cart-item-interactive"
 import { type TAddProduct } from "@components/product/types"
 
 import { useCartManager } from "./manager"
 
 export const CartPageComponent = () => {
-	const { products, addProduct } = useCartManager()
+	const { products, addProduct, removeProduct } = useCartManager()
 
 	const handleSubmit = (product: TAddProduct) => {
 		addProduct(product)
-		console.log(product)
+	}
+
+	const handleRemove = (id: string) => () => {
+		removeProduct(id)
 	}
 
 	return (
@@ -19,7 +22,12 @@ export const CartPageComponent = () => {
 			<AddProductForm onSubmit={handleSubmit} />
 
 			{products.map((product, index) => (
-				<CartItem product={product} key={product.id} no={index + 1} />
+				<CartItemInteractive
+					product={product}
+					no={index + 1}
+					onRemove={handleRemove(product.id)}
+					key={product.id}
+				/>
 			))}
 		</>
 	)
