1f62474ac736c8cc68e86386cff5703cc6afb573
Michał Grzelak <grzelakm@outlook.com>
2024-03-14 12:41:00 +0100

    Validate min name length

--- src/components/product/constants.ts
+++ src/components/product/constants.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 export const addProductToCartFormSchema = z.object({
-	name: z.string().max(20),
+	name: z.string().max(20).min(1),
 	price: z.coerce.number().positive(),
 	requiresShipping: z.boolean(),
 })
